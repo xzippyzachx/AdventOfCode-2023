4a5f81ea24a627b6c14758285adad891692f300e
Zachary Ross <zacharyross1234@gmail.com>
2023-12-03 13:03:29 -0500

    Day 3 Puzzel 1

---
+++ Day3/Puzzel1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Day 3
+// Puzzel 1
+
+string[] lines = File.ReadAllLines("Puzzel2/input.txt");
+
+bool HasAdjacentSymbol (char[] aboveLine, char[] line, char[] belowLine)
+{
+
+    return false;
+}
+
+int sum = 0;
+for (int l = 0; l < lines.Length; l++)
+{
+    char[] line = lines[l].ToCharArray();
+    for (int c = 0; c < line.Length; c++)
+    {
+        if (line[c] == '*')
+        {
+            
+        }
+    }
+}
+
+Console.WriteLine(sum);
