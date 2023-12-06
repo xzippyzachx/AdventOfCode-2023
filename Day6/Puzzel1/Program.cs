a25fdb6af2a2404003c1ee3f3d7705f2b8b6f6e3
Zachary Ross <zacharyross1234@gmail.com>
2023-12-06 10:31:00 -0500

    Day 6 puzzel 1

---
+++ Day6/Puzzel1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Day 6
+// Puzzel 1
+
+using System.Text.RegularExpressions;
+
+string[] lines = File.ReadAllLines("Puzzel1/input.txt");
+
+int[] times = Array.ConvertAll(Regex.Replace(lines[0], @"\s+", " ").Split(":")[1].Trim().Split(" "), s => int.Parse(s));
+int[] distances = Array.ConvertAll(Regex.Replace(lines[1], @"\s+", " ").Split(":")[1].Trim().Split(" "), s => int.Parse(s));
+
+int totalMulti = 1;
+for (int t = 0; t < times.Length; t++)
+{
+    int winAmount = 0;
+    for (int i = 0; i <= times[t]; i++)
+    {
+        int timeHold = i;
+        int timeMove = times[t] - i;
+        int distance = timeMove * timeHold;
+        if (distance > distances[t])
+        {
+            winAmount++;
+        }
+    }
+    totalMulti *= winAmount;
+}
+
+Console.WriteLine(totalMulti);
