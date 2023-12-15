1804b2f60c99f6d051a1f2fdda630bbad5a4cf45
Zachary Ross <zacharyross1234@gmail.com>
2023-12-15 10:22:07 -0500

    Day 15 part 2

---
+++ Day15/Part2/Program.cs
@@ -0,0 +1,73 @@
+﻿// Day 15
+// Part 2
+
+string[] lines = File.ReadAllLines("Part2/input.txt");
+
+string[] values = lines[0].Split(",");
+
+int Hash(string val)
+{
+    int curr = 0;
+    foreach (char c in val)
+    {
+        curr += (int)c;
+        curr *= 17;
+        curr = curr % 256;
+    }
+    return curr;
+}
+
+Dictionary<int, List<Lens>> boxs = new Dictionary<int, List<Lens>>();
+foreach (string val in values)
+{
+    char op = val.Contains("=") ? '=' : '-';
+    string label = op == '=' ? val.Split("=")[0] : val.Split("-")[0];
+    int focal = op == '=' ? int.Parse(val.Split("=")[1]) : 0;
+
+    int hash = Hash(label);
+
+    if (op == '-')
+    {
+        if (boxs.ContainsKey(hash))
+        {
+            int index = boxs[hash].FindIndex(l => l.label == label);
+            if (index != -1)
+            {
+                boxs[hash].RemoveAt(index);
+            }
+        }
+    }
+    else if (op == '=')
+    {
+        Lens lens = new Lens(label, focal);
+        if (boxs.ContainsKey(hash))
+        {
+            int index = boxs[hash].FindIndex(l => l.label == label);
+            if (index != -1)
+            {
+                boxs[hash].RemoveAt(index);
+                boxs[hash].Insert(index, lens);
+            }
+            else
+            {
+                boxs[hash].Add(lens);
+            }
+        }
+        else
+        {
+            boxs.Add(hash, new List<Lens>{ lens });
+        }
+    }
+}
+
+int sum = 0;
+foreach (int hash in boxs.Keys)
+{
+    for(int slot = 0; slot < boxs[hash].Count; slot++)
+    {
+        sum += (hash + 1) * (slot + 1) * boxs[hash][slot].focal;
+    }
+}
+Console.WriteLine(sum);
+
+record Lens(string label, int focal);
