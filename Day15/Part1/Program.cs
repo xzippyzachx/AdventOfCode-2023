b8193e3fb339049f8648f773bc79112bf1773b1a
Zachary Ross <zacharyross1234@gmail.com>
2023-12-15 09:40:13 -0500

    Day 15 part 1

---
+++ Day15/Part1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Day 15
+// Part 1
+
+string[] lines = File.ReadAllLines("Part1/input.txt");
+
+string[] values = lines[0].Split(",");
+int sum = 0;
+foreach (string val in values)
+{
+    int curr = 0;
+    foreach (char c in val)
+    {
+        curr += (int)c;
+        curr *= 17;
+        curr = curr % 256;
+    }
+    sum += curr;
+}
+Console.WriteLine(sum);
